26d2c3de5e1c86553233dad7b246bd8a23571353
dunnywei <wei_don@yahoo.com>
2018-07-12 20:11:47 -0400

    (4:46)

---
+++ lecture_29_pthread.c
@@ -0,0 +1,24 @@
+//C Programming in Linux Tutorial #029 - pthreads
+//https://www.youtube.com/watch?v=n9IT5RAludA&list=PLypxmOPCOkHXbJhUgjRaV2pD9MJkIArhg&t=0s&index=30
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(int argc,char* argv[])
+{
+   //lecture_29 (2:01)
+   pthread_t thread1,thread2;
+   char *msg1="first thread";
+   char *msg2="second thread";
+   int ret1,ret2;
+   ret1=pthread_create(thread1,NULL, myfunc,(void *)msg1);
+   ret2=pthread_create(thread1,NULL, myfunc,(void *)msg2);
+   //4:46
+   printf("First  thread ret1=%d\n",ret1);
+   printf("Second thread ret2=%d\n",ret2);
+   
+   
+
+   pthread_join(thread1,NULL);
+   pthread_join(thread2,NULL);
+}
