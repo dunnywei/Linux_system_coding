8753f660552141f9884ed71c6f8ef4cac3c6fe9f
dunnywei <wei_don@yahoo.com>
2018-09-22 12:50:53 -0400

    (2:52)

--- lecture_36_shared_memory.c
+++ lecture_36_shared_memory.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/type.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHSIZE 100
+
+int main(int argc, char *argv[])
+{
+    int shmid;
+    key_t key;
+    char *shm;
+     
+    key=9976;
+    printf("Hello World");
+
+}
 
 //(1:00)
